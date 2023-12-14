3636fc98be388208eb799c3b197fc7f479ab94a7
MajemiteJames <majemiteokoro@gmail.com>
2023-12-14 17:38:42 +0100

    connected database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ReservationModule } from './reservation/reservation.module';
 
 @Module({
-  imports: [ReservationModule],
+  imports: [
+    ReservationModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'ruby.db.elephantsql.com',
+      username: 'ktroebhw',
+      password: 'Zs-AE58bpN2c5gWPz_GhVlHEkbD-SKmS',
+      database: 'ktroebhw',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
