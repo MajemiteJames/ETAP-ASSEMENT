dd2d924b8d897a9d77e0296dd3b52aedb758f391
MajemiteJames <majemiteokoro@gmail.com>
2023-12-14 13:17:50 +0100

    added validation to create reservation

--- src/reservation/dto/create-reservation.dto.ts
+++ src/reservation/dto/create-reservation.dto.ts
@@ -1,5 +1,15 @@
+import { IsDateString, IsNotEmpty, IsString } from 'class-validator';
+
 export class createReservationDto {
+  @IsNotEmpty()
+  @IsString()
   room_type: string;
+
+  @IsNotEmpty()
+  @IsDateString()
   expected_checkin_time: Date;
+
+  @IsNotEmpty()
+  @IsDateString()
   expected_checkout_time: Date;
 }
